4a1c586e5a0a8c48187124ae9c03b605fe1169fb
John Grinwis <john.grinwis@gmail.com>
2021-06-14 16:39:28 +0200

    created a good and bad endpoint

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/error')
 def server_error():
    return 'server error', 500
-    
+
+@app.route('/good')
+def hello_world():
+   return 'hello world!', 200
