2d3d3d82676867408b242497f554cffca912b858
John Grinwis <john.grinwis@gmail.com>
2021-06-16 10:10:46 +0200

    changed random endpoint

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ from flask import Flask
 from random import randint
 app = Flask(__name__)
 
+
 @app.route('/error/')
 def server_error():
-   return 'flask server error', 500
+    return 'flask server error', 500
+
 
 @app.route('/good/')
 def hello_world():
-   return 'flask hello world!', 200
+    return 'flask hello world!', 200
+
 
 @app.route('/random/')
-def hello_world():
-   if randint(0,1):
-      return "random ok flask", 200
-   else:
-      return 'random error flask', 500
+def random():
+    if randint(0, 1):
+        return "random ok flask", 200
+    else:
+        return 'random error flask', 500
